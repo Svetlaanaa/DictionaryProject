7a408e794d3a50a1c3f4d998270e1c07aa6bebc2
Svetlana <hristich.svetlana@gmail.com>
2024-04-14 22:09:18 +0400

    подключение спринга

---
+++ src/main/java/ru/teamscore/dictionary/DictionaryApplication.java
@@ -0,0 +1,48 @@
+package ru.teamscore.dictionary;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.cfg.Configuration;
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.teamscore.dictionary.entities.Definition;
+import ru.teamscore.dictionary.entities.OtherForms;
+import ru.teamscore.dictionary.entities.Synonym;
+import ru.teamscore.dictionary.entities.Word;
+
+@SpringBootApplication
+public class DictionaryApplication {
+    public static void main(String[] args) {
+        // RestartClassLoader иногда вызывает конфликты классов при использовании java.util.stream
+        // из-за загрузки в два разных class loader. Пример ошибки:
+        // class ru.teamscore.java23.orders.model.statistics.OrdersStatistics cannot be cast
+        // to class ru.teamscore.java23.orders.model.statistics.OrdersStatistics
+        // (ru.teamscore.java23.orders.model.statistics.OrdersStatistics is in unnamed module of loader 'app';
+        // ru.teamscore.java23.orders.model.statistics.OrdersStatistics is in unnamed module of loader
+        // org.springframework.boot.devtools.restart.classloader.RestartClassLoader @61589606)
+        System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(DictionaryApplication.class, args);
+    }
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+        return new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Word.class)
+                .addAnnotatedClass(OtherForms.class)
+                .addAnnotatedClass(Synonym.class)
+                .addAnnotatedClass(Definition.class)
+                .buildSessionFactory();
+    }
+
+    @Bean
+    public EntityManager entityManager(EntityManagerFactory entityManagerFactory) {
+        return entityManagerFactory.createEntityManager();
+    }
+
+}
