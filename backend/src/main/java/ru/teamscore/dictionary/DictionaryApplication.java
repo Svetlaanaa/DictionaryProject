d0c0ca64428d66cc6ffc53e1fbbef2c1437c0516
Svetlana <hristich.svetlana@gmail.com>
2024-04-24 23:11:28 +0400

    добавлен модуль фронтенд

--- backend/src/main/java/ru/teamscore/dictionary/DictionaryApplication.java
+++ backend/src/main/java/ru/teamscore/dictionary/DictionaryApplication.java
@@ -6,6 +6,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DictionaryApplication {
     public static void main(String[] args) {
         SpringApplication.run(DictionaryApplication.class, args);
-
     }
 }
